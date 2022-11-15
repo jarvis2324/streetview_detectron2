0fdd9ebdf9c532e56dd3b2e6efc031ced17e6260
jarvis2324 <gauravbarua614@gmail.com>
2022-11-15 16:41:59 +0530

    Added Three Places

--- detectron2/main.py
+++ detectron2/main.py
@@ -1,11 +1,20 @@
 from detectron2_run import pred
 from street_view_class import StreetViewer
-
+import pandas as pd
 api_key = '####'
-gwu_viewer = StreetViewer(api_key=api_key,
-                        location='800 21st St NW, Washington, DC 20052')
-lat, lon = gwu_viewer.get_meta()
-im = gwu_viewer.get_pic()
-df = pred(im,lat,lon)
-print(df)
+df = df = pd.DataFrame(columns = ['Class', 'Lat', 'Long'])
+locations = ["DLF Road,Gachibowli, Hyderabad", "Mandi King, Gacchibowli, Hyderabad" , "Tenet Diagnostics, Gachibowli, Hyderabad"]
+for location in locations:
+    gwu_viewer = StreetViewer(api_key=api_key,
+                            location=location)
+    lat, lon = gwu_viewer.get_meta()
+    im = gwu_viewer.get_pic()
+    df_temp = pred(im,lat,lon)
+    print("df_temp",df_temp)
+    # if df.empty:
+    #     df = df_temp
+    # else:
+    #     print("Else")
+    df = df.append(df_temp,ignore_index=True)
+    print("F",df)
 df.to_csv('./file.csv')
--- detectron2/street_view.py
+++ detectron2/street_view.py
@@ -4,37 +4,40 @@ meta_base = 'https://maps.googleapis.com/maps/api/streetview/metadata?'
 pic_base = 'https://maps.googleapis.com/maps/api/streetview?'
 api_key = '####'
 # using my graduate school almar mater, GWU, as an example
-location = '2121 I St NW, Washington, DC 20052'
+#location = 'Mandi King,Gachibowli, Hyderabad, India'
+locations = ["DLF Road,Gachibowli, Hyderabad", "Mandi King, Gacchibowli, Hyderabad" , "Tenet Diagnostics, Gachibowli, Hyderabad"]
+for location in locations:
+    # define the params for the metadata reques
+    meta_params = {'key': api_key,
+                'location': location}
+    # define the params for the picture request
+    pic_params = {'key': api_key,
+                'location': location,
+                'size': "640x640",
+                'radius' : 1000}
+    print(pic_params)
+    # obtain the metadata of the request (this is free)
+    meta_response = requests.get(meta_base, params=meta_params)
 
-# define the params for the metadata reques
-meta_params = {'key': api_key,
-               'location': location}
-# define the params for the picture request
-pic_params = {'key': api_key,
-              'location': location,
-              'size': "640x640"}
+    print(meta_response.json())
+    #print(meta_response.json()['location'])
+    if meta_response.json()['status'] == 'OK':
+        pic_response = requests.get(pic_base, params=pic_params)
 
-# obtain the metadata of the request (this is free)
-meta_response = requests.get(meta_base, params=meta_params)
+        for key, value in pic_response.headers.items():
+            print(f"{key}: {value}")
+            
+        print(pic_response.ok)
 
-print(meta_response.json())
-print(meta_response.json()['location'])
-pic_response = requests.get(pic_base, params=pic_params)
+        with open('test.jpg', 'wb') as file:
+            file.write(pic_response.content)
+        # remember to close the response connection to the API
+        pic_response.close()
 
-for key, value in pic_response.headers.items():
-    print(f"{key}: {value}")
-    
-print(pic_response.ok)
-
-with open('test.jpg', 'wb') as file:
-    file.write(pic_response.content)
-# remember to close the response connection to the API
-pic_response.close()
-
-# using matpltolib to display the image
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-plt.figure(figsize=(10, 10))
-img=mpimg.imread('test.jpg')
-imgplot = plt.imshow(img)
-plt.show()
+        # using matpltolib to display the image
+        import matplotlib.pyplot as plt
+        import matplotlib.image as mpimg
+        plt.figure(figsize=(10, 10))
+        img=mpimg.imread('test.jpg')
+        imgplot = plt.imshow(img)
+        plt.show()
